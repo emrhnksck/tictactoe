9a1316e61436a8ff4248764859b7537a26594c12
Emrehan Kisacik <emrehankisacik@Emrehans-iMac.local>
2022-03-08 10:04:20 +0300

    created move func and controller

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,17 @@
 class Game < ApplicationRecord
+  before_validation(on: :create) do
+    self.state = {
+      0 => {0 => nil , 1 => nil , 2 => nil },
+      1 => {0 => nil , 1 => nil , 2 => nil },
+      2 => {0 => nil , 1 => nil , 2 => nil },
+    }
+    self.current_symbol = [:x, :o].sample #??
+  end
+
+  def move!(row, col)
+      state[row.to_s][col.to_s] = current_symbol
+
+  self.current_symbol = current_symbol == "x" ? "o" : "x"
+  self.save!
+  end
 end
